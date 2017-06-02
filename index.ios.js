87b4e7ec8e16d5b3230cef2a8bdcd5c3ec54aa68
Leo Picado <leopic@gmail.com>
2017-06-01 21:02:40 -0600

    Picka

--- index.ios.js
+++ index.ios.js
@@ -4,27 +4,59 @@ import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
+    DatePickerIOS,
     Button,
     Text,
     View
 } from 'react-native';
 
 export default class TiempoDeComida extends Component {
-    handleStuff() {
-        console.log('handleStuff');
+    onHandleStuff($event) {
+        console.log('onHandleStuff', $event);
+    };
+
+    onDateChange = (date) => {
+        let endDate = new Date(date);
+        endDate.setDate(endDate.getDate() + 7);
+        this.setState({ startDate: date, endDate: endDate });
+    };
+
+    static endDate() {
+        let endDate = new Date();
+        endDate.setDate(endDate.getDate() + 7);
+        return endDate;
     }
+
+    static defaultProps = {
+        startDate: new Date(),
+        endDate: TiempoDeComida.endDate()
+    };
+
+    state = {
+        startDate: this.props.startDate,
+        endDate: this.props.endDate
+    };
+
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
                     <Text style={styles.headerText}>Semana Nueva</Text>
                 </View>
+
                 <Text style={styles.welcome}>
                     Welcome to React Native!
                 </Text>
+
+                <DatePickerIOS date={this.state.startDate} mode="datetime" style={styles.datePicker}
+                               onDateChange={this.onDateChange}/>
+
+                <DatePickerIOS date={this.state.endDate} mode="datetime" style={styles.datePicker}
+                               onDateChange={function (a, b) { console.log(a, b); }}/>
+
                 <View style={styles.footer}>
-                    <Button style={styles.footerButton} title={'Cancelar'} onPress={this.handleStuff}>Cancelar</Button>
-                    <Button style={styles.footerButton} title={'Listo'} onPress={this.handleStuff}>Listo</Button>
+                    <Button style={styles.footerButton} title={'Cancelar'} onPress={this.onHandleStuff}>Cancelar</Button>
+                    <Button style={styles.footerButton} title={'Listo'} onPress={this.onHandleStuff}>Listo</Button>
                 </View>
             </View>
         );
@@ -62,6 +94,10 @@ const styles = StyleSheet.create({
         margin: 0,
         textAlign: 'center',
         width: '100%',
+    },
+    datePicker: {
+        // backgroundColor: 'pink',
+        // height: 0
     }
 });
 
