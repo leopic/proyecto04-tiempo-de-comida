9eaf32289ff4ba89b33abcfeef5b2b338fa79e42
Leo Picado <leopic@gmail.com>
2017-06-01 19:17:13 -0600

    Estado basico

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Button,
+    Text,
+    View
 } from 'react-native';
 
 export default class TiempoDeComida extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    handleStuff() {
+        console.log('handleStuff');
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>Semana Nueva</Text>
+                </View>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <View style={styles.footer}>
+                    <Button style={styles.footerButton} title={'Cancelar'} onPress={this.handleStuff}>Cancelar</Button>
+                    <Button style={styles.footerButton} title={'Listo'} onPress={this.handleStuff}>Listo</Button>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    header: {
+        alignSelf: 'flex-start',
+        backgroundColor: 'blue',
+        height: 64,
+        width: '100%',
+    },
+    headerText: {
+        color: '#fff',
+        lineHeight: 64,
+        textAlign: 'center',
+    },
+    footer: {
+        flexDirection: 'row',
+        height: 48,
+        justifyContent: 'space-around',
+    },
+    welcome: {
+        alignSelf: 'flex-end',
+        backgroundColor: 'red',
+        color: '#fff',
+        flexGrow: 2,
+        fontSize: 20,
+        margin: 0,
+        textAlign: 'center',
+        width: '100%',
+    }
 });
 
 AppRegistry.registerComponent('TiempoDeComida', () => TiempoDeComida);
